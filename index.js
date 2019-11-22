6ec3264c503f5bec63699b4cb89b20e3da06ff50
Carl RDJ <carl.rdj@gmail.com>
2019-11-22 10:58:40 -0500

    dev

--- index.js
+++ index.js
@@ -12,25 +12,35 @@ app.use(history({
 app.use(serveStatic(__dirname))
 
 app.use('/*', express.static(__dirname + '/*'))
-
-const credentials = {
-  key: fs.readFileSync('./../gamarraya-key/gamarraya.key', 'utf8'),
-  cert: fs.readFileSync('./../gamarraya-key/670cb8308ec29972.crt', 'utf8'),
-  equestCert: true,
-  ca: fs.readFileSync('./../gamarraya-key/gd_bundle-g2-g1.crt', 'utf8')
-};
+try {
+  const credentials = {
+    key: fs.readFileSync('./../gamarraya-key/gamarraya.key', 'utf8'),
+    cert: fs.readFileSync('./../gamarraya-key/670cb8308ec29972.crt', 'utf8'),
+    equestCert: true,
+    ca: fs.readFileSync('./../gamarraya-key/gd_bundle-g2-g1.crt', 'utf8')
+  };  
+  
+  const httpsServer = https.createServer(credentials, app);
+} catch (error) {
+  console.log('Falta CRT');  
+}
 
 const httpServer = http.createServer(function(req, res) {
-        res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
-        res.end();
+  res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
+  res.end();
 }, app);
-const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-const server = httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
+try {
+  const server = httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+  });
+  
+} catch (error) {
+  console.log('Falta CRT');
+  
+}
 
